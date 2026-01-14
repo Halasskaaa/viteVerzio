cf97a00a88a60c569df48ec8085a7bd541813770
Phan Thi Linh Tra <phan.thi.linh.tra@petrik.hu>
2026-01-14 12:04:04 +0100

    main.js form működtetése

--- gitVerVite/src/main.js
+++ gitVerVite/src/main.js
@@ -1,2 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
+
+const form = document.getElementById('regForm');
+const successMessage = document.getElementById('successMessage');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  form.reset();
+  successMessage.textContent = 'Sikeres regisztráció';
+});
